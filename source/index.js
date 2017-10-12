a0bea3f9bc680d7f0940408f0bd8f038af69486b
Jérémie Drouet <jeremie.drouet@gmail.com>
2017-10-12 19:50:47 +0300

    add linter

--- source/index.js
+++ source/index.js
@@ -1,9 +1,24 @@
+/**
+ * class reprensenting an entity to clean
+ */
 class Schema {
+  /**
+   * Create a schema
+   * @param {object} definition - Definition of your model
+   */
   constructor(definition) {
     this.definition = definition;
   }
 
-  cleanField(view, result, field, key, value) {
+  /**
+   * Give the change to apply to the cleaned object
+   * @param {string} view - View requested
+   * @param {object} field - Description of the field
+   * @param {string} key - Key of the object your cleaning
+   * @param {any} value - value of the object for that key
+   * @return {object} change to apply to the cleaned object
+   */
+  cleanField(view, field, key, value) {
     const canSeeField = !view || !field.views || field.views.includes(view);
     if (!canSeeField) return {};
     if (field.array) {
@@ -21,11 +36,18 @@ class Schema {
     return {[key]: value};
   }
 
+
+  /**
+   * Give the change to apply to the cleaned object
+   * @param {object|array} input - Item or list of items to clean
+   * @param {string} view - View requested
+   * @return {object} the cleaned object
+   */
   clean(input, view) {
     return Object.keys(this.definition).reduce((result, key) => {
       const field = this.definition[key];
       const value = input[key];
-      return Object.assign(result, this.cleanField(view, result, field, key, value));
+      return Object.assign(result, this.cleanField(view, field, key, value));
     }, {});
   }
 }
--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai');
+const {expect} = require('chai');
 const Schema = require('../source');
 
 const Subthing = new Schema({
@@ -7,16 +7,16 @@ const Subthing = new Schema({
 
 const Thing = new Schema({
   id: {},
-  data: { array: true, views: ['admin', 'public'] },
-  subthing: { schema: Subthing, views: ['admin']  },
-  subthings: { array: true, schema: Subthing, views: ['admin', 'public']  },
+  data: {array: true, views: ['admin', 'public']},
+  subthing: {schema: Subthing, views: ['admin']},
+  subthings: {array: true, schema: Subthing, views: ['admin', 'public']},
 });
 
 const Account = new Schema({
   id: {},
-  password: { views: [] },
-  adminThings: { array: true, schema: Thing, views: ['admin'] },
-  publicThings: { array: true, schema: Thing, views: ['admin', 'public'] },
+  password: {views: []},
+  adminThings: {array: true, schema: Thing, views: ['admin']},
+  publicThings: {array: true, schema: Thing, views: ['admin', 'public']},
 });
 
 describe('clean', () => {
@@ -25,20 +25,20 @@ describe('clean', () => {
       id: 1,
       password: 'password',
       adminThings: [
-        { id: 2, data: ['toto'], subthing: { id: 6 } },
-        { id: 3 }
+        {id: 2, data: ['toto'], subthing: {id: 6}},
+        {id: 3},
       ],
-      publicThings: [{ id: 4, subthings: [ { id: 7 } ] }, { id: 5 }],
+      publicThings: [{id: 4, subthings: [{id: 7}]}, {id: 5}],
     })).to.eql({
       id: 1,
       password: 'password',
       adminThings: [
-        { id: 2, data: ['toto'], subthing: { id: 6 }, subthings: [] },
-        { id: 3, data: [], subthing: null, subthings: [] },
+        {id: 2, data: ['toto'], subthing: {id: 6}, subthings: []},
+        {id: 3, data: [], subthing: null, subthings: []},
       ],
       publicThings: [
-        { id: 4, data: [], subthing: null, subthings: [ { id: 7 } ] },
-        { id: 5, data: [], subthing: null, subthings: [] },
+        {id: 4, data: [], subthing: null, subthings: [{id: 7}]},
+        {id: 5, data: [], subthing: null, subthings: []},
       ],
     });
   });
@@ -47,12 +47,12 @@ describe('clean', () => {
     expect(Account.clean({
       id: 1,
       password: 'password',
-      publicThings: [{ id: 4 }, { id: 5 }],
+      publicThings: [{id: 4}, {id: 5}],
     }, 'public')).to.eql({
       id: 1,
       publicThings: [
-        { id: 4, data: [], subthings: [] },
-        { id: 5, data: [], subthings: [] },
+        {id: 4, data: [], subthings: []},
+        {id: 5, data: [], subthings: []},
       ],
     });
   });
